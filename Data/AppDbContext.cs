81682e377c38fc071f495166dd68e8f9cd05ce76
Le Huu Viet Hoang <viethoang232006@gmail.com>
2026-07-06 23:28:26 +0700

    register EF Core and DbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoList.Models.Entities;
+using Task = TodoList.Models.Entities.Task;
+
+namespace TodoList.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){ }
+
+        public DbSet<Task> Tasks { get; set; } = null!;
+        public DbSet<Priority> Priorities { get; set; } = null!;
+        public DbSet<Tag> Tags { get; set; } = null!;
+    }
+}
