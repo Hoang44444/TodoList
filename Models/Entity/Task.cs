3a9dcab8d88ba772c1c2c5e5cc3c4892f22e448d
Le Huu Viet Hoang <viethoang232006@gmail.com>
2026-07-06 10:18:30 +0700

    feat: add relationship from task to another

--- Models/Entity/Task.cs
+++ Models/Entity/Task.cs
@@ -1,12 +1,16 @@
-﻿namespace TodoList.Models
+﻿namespace TodoList.Models.Entity
 {
     public class Task : Entity
     {
-        public string TaskName { get; set; }
+        public string TaskName { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public int TagId { get; set; }
+        ICollection<Tag> Tags { get; set; } = new List<Tag>();
         DateTime StartDate { get; set; }
         DateTime DueDate { get; set; }
-        TimeOnly Time { get; set; }
+        public int PriorityId { get; set; }
+        public Priority Priority { get; set; } = new Priority();
         string ReferenceNote { get; set; } = string.Empty;
+
     }
 }
