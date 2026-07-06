0d4e9d2c135ff0d0c7bb52cfbb9a5e7b9454facf
Le Huu Viet Hoang <viethoang232006@gmail.com>
2026-07-06 09:58:34 +0700

    feat: add parent class Entity and model Task

---
+++ Models/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace TodoList.Models
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+    }
+}
--- Models/Task.cs
+++ Models/Task.cs
@@ -1,7 +1,12 @@
 ﻿namespace TodoList.Models
 {
-    public class Task
+    public class Task : Entity
     {
-
+        public string TaskName { get; set; }
+        public string Description { get; set; } = string.Empty;
+        DateTime StartDate { get; set; }
+        DateTime DueDate { get; set; }
+        TimeOnly Time { get; set; }
+        string ReferenceNote { get; set; } = string.Empty;
     }
 }
