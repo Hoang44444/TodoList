5b8af1ea9fd03a009425959b2eb3d71f1ecef29e
Le Huu Viet Hoang <viethoang232006@gmail.com>
2026-07-06 10:30:11 +0700

    feat: add status for the task

--- Models/Entity/Task.cs
+++ Models/Entity/Task.cs
@@ -1,4 +1,6 @@
-﻿namespace TodoList.Models.Entity
+﻿using TodoList.Models.Status;
+
+namespace TodoList.Models.Entity
 {
     public class Task : Entity
     {
@@ -6,11 +8,11 @@
         public string Description { get; set; } = string.Empty;
         public int TagId { get; set; }
         ICollection<Tag> Tags { get; set; } = new List<Tag>();
-        DateTime StartDate { get; set; }
-        DateTime DueDate { get; set; }
+        DateTime? StartDate { get; set; }
+        DateTime? DueDate { get; set; }
         public int PriorityId { get; set; }
         public Priority Priority { get; set; } = new Priority();
         string ReferenceNote { get; set; } = string.Empty;
-
+        public TodoStatus Status { get; set; } = TodoStatus.Pending;
     }
 }
