9075ae49f7f35bd7dedc8392ec09948a0f4efa1e
Le Huu Viet Hoang <viethoang232006@gmail.com>
2026-07-06 10:13:43 +0700

    feat: add columns CreatedAt and UpdateAt in class Entity

--- Models/Entity.cs
+++ Models/Entity.cs
@@ -3,5 +3,7 @@
     public class Entity
     {
         public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
     }
 }
