25b30ab5cb9775d977dd225a9560d726e758ff01
Le Huu Viet Hoang <viethoang232006@gmail.com>
2026-07-10 01:13:25 +0700

    feat: implement tag repository

---
+++ Repository/TagRepository.cs
@@ -0,0 +1,12 @@
+﻿using TodoList.Data;
+using TodoList.Models.Entities;
+
+namespace TodoList.Repository
+{
+    public class TagRepository : GenericRepository<Tag>, ITagRepository
+    {
+        public TagRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
